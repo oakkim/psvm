02df2ec0cd98c36908dbeac3a808b98c709b6a26
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-03-17 02:10:56 +0900

    Feat: 헤더 로고에 메인으로 가는 링크 넣음

--- components/base/Header/index.tsx
+++ components/base/Header/index.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="p-3 pl-4 bg-white border-b flex items-center space-x-4 fixed w-full z-50">
       <div>
-        psvm
+        <a href="/">psvm</a>
       </div>
     </div>
   );
