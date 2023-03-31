d3075d15b049d946c12f492eda07a27b0fb4648c
KimDaeyong <dev.kimdaeyong@gmail.com>
2023-03-31 10:37:57 +0900

    feat: add ga4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SC1VBL8T27"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-SC1VBL8T27');
+            `
+          }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
