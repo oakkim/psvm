d213e9cf1d2d9a30fbf88cce6595a98dfa4cccdf
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-08-11 08:46:29 +0900

    feat: 임시 Supacus 적용

---
+++ components/Supacus/index.tsx
@@ -0,0 +1,84 @@
+// 출처: https://bepyan.github.io/blog/nextjs-blog/6-comments
+// import { useTheme } from 'next-themes';
+import { IframeHTMLAttributes, useEffect, useRef } from 'react';
+
+interface SupacusProps {
+  siteId: number,
+  contentId: string
+}
+
+export default function Supacus({ siteId, contentId } : SupacusProps) {
+  const divRef = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLIFrameElement>(null);
+  // const { resolvedTheme } = useTheme();
+
+  // https://github.com/giscus/giscus/tree/main/styles/themes
+  // const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
+
+  useEffect(() => {
+    // if (!ref.current || ref.current.hasChildNodes()) return;
+
+    if (!!document.getElementById('supacus-script')) {
+      return;
+    }
+
+    const scriptElem = document.createElement('script')
+    scriptElem.id = 'supacus-script'
+    scriptElem.async = true;
+    scriptElem.setAttribute('data-site-id', siteId.toString());
+    scriptElem.setAttribute('data-content-id', encodeURIComponent(contentId));
+    // scriptElem.crossOrigin = 'anonymous';
+    // scriptElem.src = 'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.6/js/iframeResizer.min.js'
+    scriptElem.innerHTML = `
+      (function() {
+        const self = document.currentScript;
+        const siteId = self.getAttribute("data-site-id");
+        const contentId = self.getAttribute("data-content-id");
+      
+        const iframeResizer = document.createElement('script');
+        iframeResizer.async = true;
+        iframeResizer.crossOrigin = 'anonymous';
+        iframeResizer.src = 'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.6/js/iframeResizer.min.js'
+      
+        iframeResizer.onload = function() {
+          const iframe = document.createElement('iframe');
+      
+          iframe.id = 'supacus'
+          iframe.src = \`https://supacus.offwork.kr/sites/\${siteId}/contents/\${contentId}\`
+          iframe.onload = function() {
+            iFrameResize({}, '#supacus');
+          }
+          self.parentElement.appendChild(iframe);
+        };
+        console.log(self.parentElement)
+        self.parentElement.appendChild(iframeResizer);
+      })();
+    `;
+    divRef?.current?.appendChild(scriptElem)
+
+    // iFrameResize({}, "#supacus")
+  });
+
+  // https://github.com/giscus/giscus/blob/main/ADVANCED-USAGE.md#isetconfigmessage
+  // useEffect(() => {
+    // const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    // iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app');
+  // }, [theme]);
+
+  // useEffect(() => {
+  //   if (!!ref.current?.style) {
+  //     ref.current.style.height = (ref?.current?.contentWindow?.document.documentElement.scrollHeight ?? 0) + 'px';
+  //   }
+  // }, [ref?.current?.contentWindow?.document.documentElement.scrollHeight])
+
+  // return <section ref={ref} />;
+  console.log(contentId, encodeURIComponent(contentId))
+  return (
+    <div ref={divRef} className="flex flex-col items-stretch">
+      {/* <iframe id="supacus" ref={ref} src={"https://supacus.offwork.kr/sites/2/contents/" + encodeURIComponent(contentId)}
+        onLoad={() => {
+          
+        }}/> */}
+    </div>
+  )
+}
--- pages/posts/[...id].tsx
+++ pages/posts/[...id].tsx
@@ -14,6 +14,7 @@ import style from './posts.module.scss';
 import Giscus from '@/components/Giscus';
 import Separator from '@/components/post/Separator';
 import useTableOfContents from '@/hooks/useTableOfContents';
+import Supacus from '@/components/Supacus';
 
 const PostDetailPage = ({
   post,
@@ -44,7 +45,7 @@ const PostDetailPage = ({
           {post?.body.code}
         </PostBody>
         <Separator/>
-        <Giscus/>
+        <Supacus siteId={1} contentId={post?._raw.flattenedPath}/>
       </div>
    </div>
   );
