a8daca10a2046fa0e3a167a395e30ff1bd623f8a
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-08-22 22:56:59 +0900

    feat: sitemap.xml 추가

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,49 @@
+import { Post, allPosts } from 'contentlayer/generated';
+import dayjs from 'dayjs';
+import { GetServerSideProps } from 'next';
+
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+function generateSiteMap(posts: Post[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://jsonplaceholder.typicode.com</loc>
+     </url>
+     <url>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+     </url>
+     ${posts
+       .map(({ _raw, updatedAt }) => {
+         return `
+       <url>
+           <loc>${`${PUBLIC_URL}/${_raw.flattenedPath}`}</loc>
+           <lastmod>${dayjs(updatedAt).format("YYYY-MM-DD")}</lastmod>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(allPosts);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
