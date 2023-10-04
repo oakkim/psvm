eaa9ecbc25f1d9984be730979f68db26d41820f6
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-10-04 23:51:51 +0900

    refactor: 아코디언 컴포넌트의 여백 및 보더 조정

--- components/Accordian/index.tsx
+++ components/Accordian/index.tsx
@@ -9,12 +9,12 @@ const Accordian = ({title, children}: AccordianType) => {
   const [visible, setVisible] = useState<boolean>(false)
 
   return (
-    <div>
-      <div className="cursor-pointer select-none" onClick={() => setVisible(!visible)}>
+    <div className="mb-5">
+      <div className="cursor-pointer select-none mb-5" onClick={() => setVisible(!visible)}>
         <span className="font-mono">{visible ? "▼" : "▶"}</span>
         <span className="ml-2">{title}</span>
       </div>
-      <div className={visible ? "" : "hidden"}>
+      <div className={`${visible ? "" : "hidden"} p-5 pb-0 border`}>
         {children}
       </div>
     </div>
