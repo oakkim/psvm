cffab61cb3bd0b86fde422231dc777feb5ae1ed2
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-10-07 23:24:15 +0900

    style: 로고 스타일 변경

--- components/base/Header/index.tsx
+++ components/base/Header/index.tsx
@@ -4,7 +4,11 @@ const Header = () => {
   return (
     <div className="p-3 pl-4 bg-white border-b flex items-center space-x-4 fixed w-full z-50">
       <div>
-        <Link href="/">psvm</Link>
+        <Link href="/">
+          <div className="flex justify-center items-center">
+            <code>psvm {"{}"}</code>
+          </div>
+        </Link>
       </div>
     </div>
   );
